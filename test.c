075a815a49674af21e8dc0438606f08ec93ecaf8
Hamza Rami <ramihamza504@gmail.com>
2025-02-18 15:39:35 +0100

    update

--- test.c
+++ test.c
@@ -1,35 +1,65 @@
-#include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 
 int main()
 {
 	int p[2];
-	int i;
-	int f;
-	char str[11];
+	int p2[2];
+	int f1, f2, f3;
+	char *cmd1[] = {"/bin/ls", "-l", NULL};
+	char *cmd2[] = {"/usr/bin/grep", ".c", NULL};
+	char *cmd3[] = {"/usr/bin/wc", "-l", NULL};
 
-	i = pipe(p);
-	if( i < 0)
+	if (pipe(p) < 0 || pipe(p2) < 0)
 	{
-		perror("error no pipe\n");
-		return (0);
+		perror("Pipe error");
+		return (1);
 	}
-	f = fork();
-	dup2(p[1], 1);
-	if (f == 0)
+	f1 = fork();
+	if (f1 == 0)
 	{
-		close(p[1]);
-		read(p[0], str, 10);
-		str[10] = '\0';
-		printf("%s", str);
-	}
-	else
-	{
+		dup2(p[1], 1);
 		close(p[0]);
-		write(p[1], "hiim rami", 10);
+		close(p[1]);
+		close(p2[0]);
+		close(p2[1]);
+		execve(cmd1[0], cmd1, NULL);
+		perror("execve ls failed");
+		return (1);
 	}
+	f2 = fork();
+	if (f2 == 0)
+	{
+		dup2(p[0], 0);
+		dup2(p2[1], 1); 
+		close(p[0]);
+		close(p[1]);
+		close(p2[0]);
+		close(p2[1]);
+		execve(cmd2[0], cmd2, NULL);
+		perror("execve grep failed");
+		return (1);
+	}
+	f3 = fork();
+	if (f3 == 0)
+	{
+		dup2(p2[0], 0);
+		close(p2[0]);
+		close(p2[1]);
+		close(p[0]);
+		close(p[1]);
+		execve(cmd3[0], cmd3, NULL);
+		perror("execve wc failed");
+		return (1);
+	}
+	close(p[0]);
+	close(p[1]);
+	close(p2[0]);
+	close(p2[1]);
+	waitpid(f1, NULL, 0);
+	waitpid(f2, NULL, 0);
+	waitpid(f3, NULL, 0);
 	return 0;
 }
