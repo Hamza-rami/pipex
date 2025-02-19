96131ec9081005975838df32e7faf63e76632146
Hamza Rami <ramihamza504@gmail.com>
2025-02-19 13:44:12 +0100

    update

---
+++ pipex.h
@@ -0,0 +1,14 @@
+#ifndef PIPEX_H
+#define PIPEX_H
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+char	**ft_split(char const *s, char c);
+
+#endif
--- test.c
+++ test.c
@@ -1,65 +1,23 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdio.h>
+#include "pipex.h"
 
-int main()
+int main(int ac, char *av[])
 {
-	int p[2];
-	int p2[2];
-	int f1, f2, f3;
-	char *cmd1[] = {"/bin/ls", "-l", NULL};
-	char *cmd2[] = {"/usr/bin/grep", ".c", NULL};
-	char *cmd3[] = {"/usr/bin/wc", "-l", NULL};
+	char	*file1;
+	char	*file2;
+	char	**cmd1;
+	char	**cmd2;
+	int		pip[2];
 
-	if (pipe(p) < 0 || pipe(p2) < 0)
+	if(ac != 5)
 	{
-		perror("Pipe error");
-		return (1);
+		perror("Error \n enter the argument exact");
+		exit(0);
 	}
-	f1 = fork();
-	if (f1 == 0)
-	{
-		dup2(p[1], 1);
-		close(p[0]);
-		close(p[1]);
-		close(p2[0]);
-		close(p2[1]);
-		execve(cmd1[0], cmd1, NULL);
-		perror("execve ls failed");
-		return (1);
-	}
-	f2 = fork();
-	if (f2 == 0)
-	{
-		dup2(p[0], 0);
-		dup2(p2[1], 1); 
-		close(p[0]);
-		close(p[1]);
-		close(p2[0]);
-		close(p2[1]);
-		execve(cmd2[0], cmd2, NULL);
-		perror("execve grep failed");
-		return (1);
-	}
-	f3 = fork();
-	if (f3 == 0)
-	{
-		dup2(p2[0], 0);
-		close(p2[0]);
-		close(p2[1]);
-		close(p[0]);
-		close(p[1]);
-		execve(cmd3[0], cmd3, NULL);
-		perror("execve wc failed");
-		return (1);
-	}
-	close(p[0]);
-	close(p[1]);
-	close(p2[0]);
-	close(p2[1]);
-	waitpid(f1, NULL, 0);
-	waitpid(f2, NULL, 0);
-	waitpid(f3, NULL, 0);
-	return 0;
+	file1 = av[1];
+	cmd1 = ft_split(av[2], ' ');
+	cmd2 = ft_split(av[3], ' ');
+	file2 = av[4];  
+	// check is command found
+	
+	return (0);
 }
